4d83da5032544da29dba3fc45c395e24a2aa3a0b
Adam Vartanian <flooey@gmail.com>
2022-05-30 21:14:17 +0100

    Add load/save, some QOL improvements
    Also changed the snippet boundary finding in an attempt to speed
    things up, but it didn't really have any effect.

--- process.py
+++ process.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+import argparse
 from collections import namedtuple
 import itertools
+import json
 import re
 import sys
 import xml.etree.ElementTree as ET
 
 WordData = namedtuple('WordData', ['word', 'date', 'snippet'])
+
+def eprint(*args, **kwargs):
+  print(*args, file=sys.stderr, **kwargs)
 
 def words_for_file(filename, data={}):
   tree = ET.parse(open(filename))
@@ -36,7 +41,8 @@ def text_for_holder(holder):
       text += " "
   return text
 
-NON_WORDS = re.compile(r"[^\w'-]+")
+NON_WORD_CHAR = r"[^\w'-]"
+NON_WORDS = re.compile(f"{NON_WORD_CHAR}+")
 
 def tokenize(text, data, date):
   lowertext = text.casefold()
@@ -45,20 +51,32 @@ def tokenize(text, data, date):
   for w in words:
     if w in data and data[w].date <= date:
       continue
-    m = re.search(f"(^|[^\\w'-])({w})([^\\w'-]|$)", lowertext)
+    m = re.search(f"(^|{NON_WORD_CHAR})({w})({NON_WORD_CHAR}|$)", lowertext)
     if not m:
       raise Exception(f'WTF? {w} ||| {text}')
-    start = m.start(2)
-    while is_snip_okay(lowertext, start):
-      start -= 1
-    end = m.end(2)
-    while is_snip_okay(lowertext, end):
-      end += 1
+    start = find_snip_boundary(lowertext, m.start(2), -1)
+    end = find_snip_boundary(lowertext, m.end(2)-1, 1)
     snippet = text[start+1:end].strip()
     if end < len(text) and text[end] == '.':
       snippet += '.'
     result[w] = snippet
   return result
+
+def find_snip_boundary(lowertext, index, dir):
+  while 0 <= (index + dir) < len(lowertext):
+    index += dir
+    c = lowertext[index]
+    if 'a' <= c <= 'z' or '0' <= c <= '9' or c in ":; ',-()":
+      continue
+    if c == '.':
+      if index >= 2 and lowertext[index-2] == ' ' and 'a' <= lowertext[index-1] <= 'z':
+        continue
+      if index >= 2 and lowertext[index-2:index] == 'mr':
+        continue
+      if index >= 3 and (lowertext[index-3:index] == 'hon' or lowertext[index-3:index] == 'esq'):
+        continue
+    break
+  return index
 
 def is_snip_okay(lowertext, index):
   if index < 0 or len(lowertext) <= index:
@@ -76,10 +94,22 @@ def is_snip_okay(lowertext, index):
   return False
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('files', nargs='+')
+  parser.add_argument('--save', action='store_true')
+  parser.add_argument('--load', action='store_true')
+  args = parser.parse_args()
   data = {}
-  for f in sys.argv[1:]:
+  if args.load:
+    loaded = json.load(open('data.json'))
+    for k in loaded:
+      data[k] = WordData(*loaded[k])
+  for f in args.files:
+    eprint(f)
     words_for_file(f, data)
   print(len(data))
+  if args.save:
+    json.dump(data, open('data.json', 'w'), indent=2, sort_keys=True)
 
 if __name__ == '__main__':
   main()
